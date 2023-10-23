81a0f91e4e7a6d1e4da4a5bf916752d00039c5a1
nawel-boudjellal <147946822+nawel-boudjellal@users.noreply.github.com>
2023-10-23 10:17:54 +0100

    Add files via upload

---
+++ depl.m
@@ -0,0 +1,15 @@
+%le pas de déplacement%
+function pasp=depl(x,deltax)
+%min=0.99;
+min=1;
+n=length(x);
+for i=1:n
+    if deltax(i)<0
+        if min>(-x(i)/deltax(i))
+            min=-x(i)/deltax(i);
+        end
+    end
+end
+pasp=min;
+end
+        
