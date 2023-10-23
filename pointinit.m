c71532c154393ada90c14388cc5e78418de0bbf1
nawel-boudjellal <147946822+nawel-boudjellal@users.noreply.github.com>
2023-10-23 21:41:36 +0200

    Update pointinit.m

--- pointinit.m
+++ pointinit.m
@@ -1,10 +1,9 @@
 function [s]=pointinit(A,b1)    
-%    Le point initiale primale vérifie Ax=b1,x>=0
+%   The initial primal point checks Ax=b1,x>=0
 % A=[1 1 1 1;1 1 0 -3];
 % b1=[1;0.5];
 % c=[1;2;3;4];
 n=size(A,2);
-% donner la point initial x0>0
 x=ones(n,1);
 x(n+1)=1;
 %%%%
@@ -12,7 +11,6 @@ An=[A,b1-A*x(1:n)];
 c1=[zeros(n,1);1];
 n=n+1;
 g=0;
-% la procedure de Karmarkar généralisé
  epsilon=10^(-4);
  %alpha=1/4;
  alpha=n/(2*n-1);
